5c5ee35454e7dc60b784c7cb1ac45c21cb9c886b
Fujiwara Bach <fujiwarabach@outlook.com>
2025-11-18 20:04:48 +0800

    Update AppBuilderExtension.cs

--- v2rayN/v2rayN.Desktop/Common/AppBuilderExtension.cs
+++ v2rayN/v2rayN.Desktop/Common/AppBuilderExtension.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using Avalonia;
 using Avalonia.Media;
@@ -13,56 +10,20 @@ public static class AppBuilderExtension
     {
         var notoSansSc = new FontFamily(Path.Combine(Global.AvaAssets, "Fonts#Noto Sans SC"));
 
-        var fallbacks = new List<FontFallback>
+        var fallbacks = new[]
         {
-            new() { FontFamily = notoSansSc }
+            new FontFallback { FontFamily = notoSansSc },
+
+            OperatingSystem.IsLinux()
+                ? new FontFallback { FontFamily = new FontFamily("Noto Color Emoji") }
+                : null
         };
 
-        if (OperatingSystem.IsLinux())
-        {
-            var emojiFamily = DetectLinuxEmojiFamily();
-            if (!string.IsNullOrWhiteSpace(emojiFamily))
-            {
-                fallbacks.Add(new FontFallback
-                {
-                    FontFamily = new FontFamily(emojiFamily)
-                });
-            }
-        }
+        var validFallbacks = fallbacks.Where(f => f is not null).ToArray()!;
 
         return appBuilder.With(new FontManagerOptions
         {
-            FontFallbacks = fallbacks.ToArray()
+            FontFallbacks = validFallbacks
         });
-    }
-
-    private static string? DetectLinuxEmojiFamily()
-    {
-        try
-        {
-            var psi = new ProcessStartInfo
-            {
-                FileName = "/bin/bash",
-                ArgumentList =
-                {
-                    "-c",
-                    "fc-match -f \"%{family[0]}\\n\" \"emoji\" | head -n 1"
-                },
-                RedirectStandardOutput = true,
-                RedirectStandardError = false,
-                UseShellExecute = false
-            };
-
-            using var p = Process.Start(psi);
-            if (p == null)
-                return null;
-
-            var output = p.StandardOutput.ReadToEnd().Trim();
-            return string.IsNullOrWhiteSpace(output) ? null : output;
-        }
-        catch
-        {
-            return null;
-        }
     }
 }
